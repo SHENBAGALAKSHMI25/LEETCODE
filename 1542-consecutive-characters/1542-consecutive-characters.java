3c36cf9ab4ec5c79f50dba002779f7904859e3da
Shenbagalakshmi <152385426+SHENBAGALAKSHMI25@users.noreply.github.com>
2025-10-26 18:56:04 +0530

    Time: 1 ms (99.55%), Space: 42.1 MB (56.54%) - LeetHub

---
+++ 1542-consecutive-characters/1542-consecutive-characters.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int maxPower(String s) {
+        int power=1;
+        int max=1;
+        for(int i=1;i<s.length();i++){
+            if(s.charAt(i)==s.charAt(i-1)){
+                power++;
+                max = Math.max(max, power);
+
+            }
+            else{
+                max=Math.max(max,power);
+                power=1;
+            }
+        }
+        return max;
+    }
+}
