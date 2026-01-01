e49d101d995baa852045dbdfc7fca0a9681719ca
Shenbagalakshmi <152385426+SHENBAGALAKSHMI25@users.noreply.github.com>
2026-01-01 11:01:41 +0530

    Time: 0 ms (100%), Space: 43.6 MB (19.41%) - LeetHub

---
+++ 0066-plus-one/0066-plus-one.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int[] plusOne(int[] digit) {
+        for (int i = digit.length - 1; i >= 0; i--) {
+            if (digit[i] < 9) {
+                digit[i]++;
+                return digit;
+            }
+            digit[i] = 0;
+        }
+
+        // Only runs if all digits were 9
+        int[] res = new int[digit.length + 1];
+        res[0] = 1;
+        return res;
+    }
+}
