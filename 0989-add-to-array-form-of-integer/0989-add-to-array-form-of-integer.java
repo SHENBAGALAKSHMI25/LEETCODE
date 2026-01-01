04e9e3b6c6d40ccf5543c59c84ba890be93c75e9
Shenbagalakshmi <152385426+SHENBAGALAKSHMI25@users.noreply.github.com>
2026-01-01 13:04:46 +0530

    Time: 2 ms (99.7%), Space: 47.6 MB (81.7%) - LeetHub

--- 0989-add-to-array-form-of-integer/0989-add-to-array-form-of-integer.java
+++ 0989-add-to-array-form-of-integer/0989-add-to-array-form-of-integer.java
@@ -2,16 +2,14 @@ class Solution {
     public List<Integer> addToArrayForm(int[] num, int k) {
         ArrayList<Integer> res = new ArrayList<>();
         int i = num.length - 1;
-
         while (i >= 0 || k > 0) {
             if (i >= 0) {
-                k += num[i];  // add current num digit to k
+                k += num[i];  
                 i--;
             }
-            res.add(k % 10);  // add last digit
-            k /= 10;          // remaining carry
+            res.add(k % 10);  
+            k /= 10;          
         }
-
         Collections.reverse(res);
         return res;
     }
