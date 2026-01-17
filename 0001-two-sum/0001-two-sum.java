d7bcb1252738b84e148d6e567c5287f294ee0b9e
Shenbagalakshmi <152385426+SHENBAGALAKSHMI25@users.noreply.github.com>
2026-01-17 22:38:27 +0530

    Time: 98 ms (5.54%), Space: 47.1 MB (55.26%) - LeetHub

--- 0001-two-sum/0001-two-sum.java
+++ 0001-two-sum/0001-two-sum.java
@@ -1,13 +1,21 @@
 class Solution {
     public int[] twoSum(int[] nums, int target) {
-        HashMap<Integer,Integer> map=new HashMap<>();
-        for(int i=0;i<nums.length;i++){
-            int c=target-nums[i];
-            if(map.containsKey(c)){
-                return new int[]{map.get(c),i};
+    //brute force
+    //frst for loop to acccess each element
+    // second use to add with the frst elemt chosen from the frst loop
+    // the both index shouldn't same
+    //using the if condition add both element frst+secd then compare it with the target if it equals pass it to the array 
+    //at final return the array
+    int arr[]=new int[2];
+    for(int i=0;i<nums.length;i++){
+        for(int j=0;j<nums.length;j++){
+            if(i==j)continue;//it starts to continue next condition 
+            if(nums[i]+nums[j]==target){
+                arr[0]=i;
+                arr[1]=j;
             }
-            map.put(nums[i],i);
         }
-        return new int[]{-1,-1};
+    }
+    return arr;
     }
 }
