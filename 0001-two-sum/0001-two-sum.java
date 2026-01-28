be5e1e8e2f185f58d297281c8dec18d7d5d59c55
Shenbagalakshmi <152385426+SHENBAGALAKSHMI25@users.noreply.github.com>
2026-01-28 07:15:15 +0530

    Time: 6 ms (47.91%), Space: 46.6 MB (84.89%) - LeetHub

--- 0001-two-sum/0001-two-sum.java
+++ 0001-two-sum/0001-two-sum.java
@@ -1,18 +1,17 @@
 class Solution {
     public int[] twoSum(int[] nums, int target) {
-    //optimal
-    HashMap<Integer,Integer>map=new HashMap<>();
-    int arr[]=new int[2];
-    for(int i=0;i<nums.length;i++){
-        int a=target-nums[i];//9-2=7 //9-7
-        if(!map.containsKey(a)){//2
-           map.put(nums[i],i);//2,0
-        }
-        else{
-            arr[0]=map.get(a);
-            arr[1]=i;
-        }
+        int arr[]=new int[2];
+   HashMap<Integer,Integer>map=new HashMap<>();
+   for(int i=0;i<nums.length;i++){
+    int n=target-nums[i];
+    if(!map.containsKey(n)){
+       map.put(nums[i],i);
     }
-    return arr;
+    else{
+        arr[0]=map.get(n);
+        arr[1]=i;
+    }
+   }
+   return arr;
     }
 }
